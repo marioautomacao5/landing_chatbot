d8ec3e192762119a1ca9a69e7e9da827d861d891
marioautomacao5 <marioautomacao5@gmail.com>
2026-03-26 21:41:28 -0300

    Ajuste para página carregar rapidamente

--- js/script.js
+++ js/script.js
@@ -1,15 +1,18 @@
 const elements = document.querySelectorAll('.fade-in');
 
-function showOnLoad() {
+function showElements() {
     elements.forEach(el => {
         const position = el.getBoundingClientRect().top;
         const screenPosition = window.innerHeight / 1.2;
 
-        if(position < screenPosition){
+        if (position < screenPosition) {
             el.classList.add('visible');
         }
     });
 }
 
-window.addEventListener('load', showOnLoad);
-window.addEventListener('scroll', showOnLoad);
+// Executa quando a página carrega
+window.addEventListener('load', showElements);
+
+// Executa quando rolar
+window.addEventListener('scroll', showElements);
